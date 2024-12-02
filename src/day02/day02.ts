879f0ee228fb4a8f7ad49023a1b17e1acf621f5a
William <wjones@clearc2.com>
2024-12-02 10:40:52 -0600

    day 2 complete

--- src/day02/day02.ts
+++ src/day02/day02.ts
@@ -3,87 +3,84 @@
 
 import { rawInput } from "./rawInput";
 
-const testInput = `7 6 4 2 1
-1 2 7 8 9
-9 7 6 2 1
-1 3 2 4 5
-8 6 4 4 1
-1 3 6 7 9`;
+const reports = rawInput.split("\n").map((line) => line.split(" ").map(Number));
 
-const testInput2 = `48 46 47 49 51 54 56
-1 1 2 3 4 5
-1 2 3 4 5 5
-5 1 2 3 4 5
-1 4 3 2 1
-1 6 7 8 9
-1 2 3 4 3
-9 8 7 6 7`;
+function checkReportIsValid(report: number[]) {
+  if (report[0] === report[1]) {
+    return { isSafe: false, index: 0 };
+  }
 
-const reports = testInput.split("\n").map((line) => line.split(" ").map(Number));
-// console.log({ reports });
+  let test = 0;
+  for (let i = 0; i < report.length - 1; i++) {
+    if (report[i] === report[i + 1]) {
+      return { isSafe: false, index: i };
+    }
 
-function checkReport(report: number[], useDampener = false) {
-  console.log(report);
-  let skips = useDampener ? 1 : 0;
-  let isIncreasing = report[0] < report[1];
-  console.log({ isIncreasing });
+    if (report[i] < report[i + 1]) {
+      test += 1;
+    } else {
+      test -= 1;
+    }
+  }
+
+  const isIncreasing = test > 0;
+
   for (let i = 0; i < report.length - 1; i++) {
     const level = report[i];
     const nextLevel = report[i + 1];
-    console.log({ level, nextLevel });
+
     if (isIncreasing && level > nextLevel) {
-      console.log("not increasing");
-      skips -= 1;
-      if (skips < 0) {
-        return false;
-      } else {
-        report.splice(i, 1);
-        i -= 1;
-      }
+      return { isSafe: false, index: i };
     }
     if (!isIncreasing && level < nextLevel) {
-      console.log("not decreasing");
-      skips -= 1;
-      if (skips < 0) {
-        return false;
-      } else {
-        report.splice(i, 1);
-        i -= 1;
-      }
+      return { isSafe: false, index: i };
     }
+
     const diff = Math.abs(level - nextLevel);
     if (diff < 1 || diff > 3) {
-      console.log(`${diff} too big`);
-      skips -= 1;
-      if (skips < 0) {
-        return false;
-      } else {
-        report.splice(i, 1);
-        console.log({ report });
-        i -= 1;
-      }
+      return { isSafe: false, index: i };
     }
   }
-  return true;
+  return { isSafe: true, index: 0 };
+}
+
+function checkReport(report: number[], isPartOne: boolean) {
+  let { isSafe, index } = checkReportIsValid(report);
+  if (isSafe) {
+    return true;
+  }
+
+  if (isPartOne) {
+    return false;
+  }
+
+  // check after removing number at the index
+  let skippedReport = report.filter((_, i) => index !== i);
+  ({ isSafe } = checkReportIsValid(skippedReport));
+  if (isSafe) {
+    return true;
+  }
+  // check after removing the number at the next index
+  skippedReport = report.filter((_, i) => index + 1 !== i);
+  ({ isSafe } = checkReportIsValid(skippedReport));
+  return isSafe;
 }
 
 let partOne = 0;
 let partTwo = 0;
-for (let i = 0; i < reports.length; i++) {
-  const report = reports[i];
-  // let isSafe = checkReport(report);
-  // if (isSafe) {
-  //   partOne += 1;
-  // }
-
-  const isSafe = checkReport(report, true);
-  console.log({ isSafe });
+reports.forEach((report) => {
+  let isSafe = checkReport(report, true);
+  if (isSafe) {
+    partOne += 1;
+  }
+  isSafe = checkReport(report, false);
   if (isSafe) {
     partTwo += 1;
   }
-}
+});
+
 console.log({ partOne });
 console.log({ partTwo });
 
-// document.getElementById("partOne")?.appendChild(document.createTextNode(partOne.toString()));
-// document.getElementById("partTwo")?.appendChild(document.createTextNode(partTwo.toString()));
+document.getElementById("partOne")?.appendChild(document.createTextNode(partOne.toString()));
+document.getElementById("partTwo")?.appendChild(document.createTextNode(partTwo.toString()));
--- src/day03/day03.ts
+++ src/day03/day03.ts
@@ -1,8 +1,7 @@
-// Advent of Code day 2
-// https://adventofcode.com/2024/day/2
+// Advent of Code day 3
+// https://adventofcode.com/2024/day/3
 
 import { rawInput } from "./rawInput";
 
 // document.getElementById("partOne")?.appendChild(document.createTextNode(partOne.toString()));
 // document.getElementById("partTwo")?.appendChild(document.createTextNode(partTwo.toString()));
-
