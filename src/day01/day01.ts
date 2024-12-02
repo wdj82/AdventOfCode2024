15209b34c75af8cf2fa89e3402651259f8a6b5a9
William <wjones@clearc2.com>
2024-12-02 12:17:12 -0600

    clean up day 1

--- src/day01/day01.ts
+++ src/day01/day01.ts
@@ -10,37 +10,31 @@ import { rawInput } from "./rawInput";
 // 3   9
 // 3   3`;
 
-const left: number[] = [];
-const right: number[] = [];
+const leftList: number[] = [];
+const rightList: number[] = [];
 const lines = rawInput.split("\n").map((line) => line.split("   "));
 lines.forEach((line) => {
-  left.push(Number(line[0]));
-  right.push(Number(line[1]));
+  leftList.push(Number(line[0]));
+  rightList.push(Number(line[1]));
 });
 
-const sortedLeft = left.toSorted((a, b) => a - b);
-const sortedRight = right.toSorted((a, b) => a - b);
+const sortedLeft = leftList.toSorted((a, b) => a - b);
+const sortedRight = rightList.toSorted((a, b) => a - b);
 
 const partOne = sortedLeft.reduce((acc, curr, index) => acc + Math.abs(curr - sortedRight[index]), 0);
 console.log({ partOne });
 
 const score = new Map<number, number>();
-let partTwo = 0;
-left.forEach((leftNumber) => {
-  let count = score.get(leftNumber) ?? -1;
-  if (count === -1) {
-    count = right.reduce((acc, rightNumber) => {
-      if (leftNumber === rightNumber) {
-        return acc + 1;
-      }
-      return acc;
-    }, 0);
-    score.set(leftNumber, count);
-  }
-  partTwo += count * leftNumber;
+rightList.forEach((number) => {
+  const count = score.get(number) ?? 0;
+  score.set(number, count + 1);
 });
+
+const partTwo = leftList.reduce((acc, leftNumber) => {
+  const count = score.get(leftNumber) ?? 0;
+  return count * leftNumber + acc;
+}, 0);
 console.log({ partTwo });
 
 document.getElementById("partOne")?.appendChild(document.createTextNode(partOne.toString()));
 document.getElementById("partTwo")?.appendChild(document.createTextNode(partTwo.toString()));
-
