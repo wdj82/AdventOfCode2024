f594e9ae91461da09eaf96f74be54fee8a4a71b5
William Jones <37749088+wdj82@users.noreply.github.com>
2024-12-10 10:33:02 -0600

    day 11 setup

---
+++ src/day11/day11.ts
@@ -0,0 +1,8 @@
+// Advent of Code day 11
+// https://adventofcode.com/2024/day/11
+
+import { rawInput } from "./rawInput";
+
+// document.getElementById("partOne")?.appendChild(document.createTextNode(partOne.toString()));
+// document.getElementById("partTwo")?.appendChild(document.createTextNode(partTwo.toString()));
+
