d0c4c6d4b3b82be2d9d17a63fe79b723ea97fae2
William <wjones@clearc2.com>
2024-12-13 15:19:32 -0600

    day 15 setup

---
+++ src/day15/day15.ts
@@ -0,0 +1,7 @@
+// Advent of Code day 15
+// https://adventofcode.com/2024/day/15
+
+import { rawInput } from "./rawInput";
+
+// document.getElementById("partOne")?.appendChild(document.createTextNode(partOne.toString()));
+// document.getElementById("partTwo")?.appendChild(document.createTextNode(partTwo.toString()));
