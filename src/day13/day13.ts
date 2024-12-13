25fb6554724219f4f0ee360cb6a81907992d2cc2
William <wjones@clearc2.com>
2024-12-13 15:18:46 -0600

    day 13 complete

--- src/day13/day13.ts
+++ src/day13/day13.ts
@@ -3,5 +3,49 @@
 
 import { rawInput } from "./rawInput";
 
-// document.getElementById("partOne")?.appendChild(document.createTextNode(partOne.toString()));
-// document.getElementById("partTwo")?.appendChild(document.createTextNode(partTwo.toString()));
+const machines = rawInput
+  .split("\n\n")
+  .map((machine) => machine.split("\n"))
+  .map((line) => {
+    const [buttonA, buttonB, prize] = line.map((m) =>
+      m
+        .split(": ")[1]
+        .split(", ")
+        .map((value) => value.slice(2))
+        .map(Number),
+    );
+
+    return {
+      buttonA: [buttonA[0], buttonA[1]],
+      buttonB: [buttonB[0], buttonB[1]],
+      prize: [prize[0], prize[1]],
+    };
+  });
+
+function minTokensToWin([ax, ay]: number[], [bx, by]: number[], [prizeX, prizeY]: number[]) {
+  const bPresses = Math.floor((ay * prizeX - ax * prizeY) / (ay * bx - ax * by));
+  const aPresses = Math.floor((prizeX - bx * bPresses) / ax);
+  if (ax * aPresses + bx * bPresses === prizeX && ay * aPresses + by * bPresses === prizeY) {
+    return aPresses * 3 + bPresses;
+  }
+  return 0;
+}
+
+let partOne = 0;
+machines.forEach(({ buttonA, buttonB, prize }) => {
+  partOne += minTokensToWin(buttonA, buttonB, prize);
+});
+console.log({ partOne });
+
+let partTwo = 0;
+machines.forEach(({ buttonA, buttonB, prize }) => {
+  partTwo += minTokensToWin(
+    buttonA,
+    buttonB,
+    prize.map((p) => p + 10000000000000),
+  );
+});
+console.log({ partTwo });
+
+document.getElementById("partOne")?.appendChild(document.createTextNode(partOne.toString()));
+document.getElementById("partTwo")?.appendChild(document.createTextNode(partTwo.toString()));
