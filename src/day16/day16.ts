3932500db387bc5886d70cb7156be23ad86d1bdc
William Jones <37749088+wdj82@users.noreply.github.com>
2024-12-14 10:55:45 -0600

    day 16 setup

---
+++ src/day16/day16.ts
@@ -0,0 +1,8 @@
+// Advent of Code day 16
+// https://adventofcode.com/2024/day/16
+
+import { rawInput } from "./rawInput";
+
+// document.getElementById("partOne")?.appendChild(document.createTextNode(partOne.toString()));
+// document.getElementById("partTwo")?.appendChild(document.createTextNode(partTwo.toString()));
+
